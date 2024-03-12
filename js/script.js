ecfce6c19d922b426e47bab782854910bea209f2
Christoph Wiesenhofer <wiesenhoferc@gmail.com>
2024-03-12 14:56:32 +0100

    Finish content and scroll to top for homepage

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* Following code has been taken from 
+https://www.w3schools.com/howto/howto_js_scroll_to_top.asp */
+
+let toTopButton = document.getElementById("back-to-top-btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTopButton.style.display = "block";
+  } else {
+    toTopButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
