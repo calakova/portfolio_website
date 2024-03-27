fdb9a1221ffc90bdb4e478ef8232edd531306797
Christoph Wiesenhofer <wiesenhoferc@gmail.com>
2024-03-27 07:53:10 +0100

    Implement dark mode

--- js/script.js
+++ js/script.js
@@ -19,3 +19,12 @@ function topFunction() {
 }
 
 // end of taken code
+
+
+function switchColorMode() {
+  if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+    document.documentElement.setAttribute('data-bs-theme', 'light');
+  } else {
+    document.documentElement.setAttribute('data-bs-theme', 'dark');
+  }
+}
